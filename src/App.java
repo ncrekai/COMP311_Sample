2e9a94a9963640be576824753b6e4276323109b4
Natalie Rekai <nrekai@my.centennialcollege.ca>
2025-04-07 20:35:37 -0400

    init project

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Hello hello = new Hello();
+        hello.display();        
+    }
+}
---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Hello {
+
+   public void display() {
+      JOptionPane.showMessageDialog(null, "Hello", "Greetings", JOptionPane.INFORMATION_MESSAGE);
+   }
+}
